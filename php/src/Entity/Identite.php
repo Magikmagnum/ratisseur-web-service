05450c853a07c3820dc99894fbc91fb297e8d072
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2023-12-31 05:51:40 +0000

    feat: ajout de la table identite

---
+++ php/src/Entity/Identite.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IdentiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IdentiteRepository::class)]
+class Identite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $sexe = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $naissanceAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $modifyAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function isSexe(): ?bool
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(?bool $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getNaissanceAt(): ?\DateTimeImmutable
+    {
+        return $this->naissanceAt;
+    }
+
+    public function setNaissanceAt(?\DateTimeImmutable $naissanceAt): self
+    {
+        $this->naissanceAt = $naissanceAt;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifyAt(): ?\DateTimeImmutable
+    {
+        return $this->modifyAt;
+    }
+
+    public function setModifyAt(?\DateTimeImmutable $modifyAt): self
+    {
+        $this->modifyAt = $modifyAt;
+
+        return $this;
+    }
+}
