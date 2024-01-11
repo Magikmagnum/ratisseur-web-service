2468da9db8a9484fdf8d57c3801578ccce94bfdf
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2024-01-11 09:17:08 +0000

    feat: ajoute des liste de Experience

--- php/src/Entity/ExperiencesListe.php
+++ php/src/Entity/ExperiencesListe.php
@@ -13,8 +13,68 @@ class ExperiencesListe
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $valide = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $modifyAt = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function isValide(): ?bool
+    {
+        return $this->valide;
+    }
+
+    public function setValide(?bool $valide): static
+    {
+        $this->valide = $valide;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifyAt(): ?\DateTimeImmutable
+    {
+        return $this->modifyAt;
+    }
+
+    public function setModifyAt(?\DateTimeImmutable $modifyAt): static
+    {
+        $this->modifyAt = $modifyAt;
+
+        return $this;
     }
 }
