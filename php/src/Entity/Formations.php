78ac487f8c9c52d89b1dae83428edcfead840b12
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2024-01-14 15:38:43 +0000

    feat: ajour de l'entité

---
+++ php/src/Entity/Formations.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FormationsRepository::class)]
+class Formations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'formations')]
+    private ?FormationsListe $label = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $debutAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $finAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $enCour = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $modifyAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?FormationsListe
+    {
+        return $this->label;
+    }
+
+    public function setLabel(?FormationsListe $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDebutAt(): ?\DateTimeImmutable
+    {
+        return $this->debutAt;
+    }
+
+    public function setDebutAt(?\DateTimeImmutable $debutAt): static
+    {
+        $this->debutAt = $debutAt;
+
+        return $this;
+    }
+
+    public function getFinAt(): ?\DateTimeImmutable
+    {
+        return $this->finAt;
+    }
+
+    public function setFinAt(?\DateTimeImmutable $finAt): static
+    {
+        $this->finAt = $finAt;
+
+        return $this;
+    }
+
+    public function isEnCour(): ?bool
+    {
+        return $this->enCour;
+    }
+
+    public function setEnCour(?bool $enCour): static
+    {
+        $this->enCour = $enCour;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifyAt(): ?\DateTimeImmutable
+    {
+        return $this->modifyAt;
+    }
+
+    public function setModifyAt(?\DateTimeImmutable $modifyAt): static
+    {
+        $this->modifyAt = $modifyAt;
+
+        return $this;
+    }
+}
